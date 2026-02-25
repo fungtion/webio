4419f39c8ee4509d8a097a303b6afb445a47b1cb
fangxin <fangxin@fangxins-bottle.local>
2026-02-25 15:51:12 +0800

    🎨 暖色主题 + 精简页脚 + 移除描述文字

--- new-post.js
+++ new-post.js
@@ -84,7 +84,7 @@ const feedXml = `<?xml version="1.0" encoding="UTF-8"?>
   <channel>
     <title>微观算力经济学</title>
     <link>${baseUrl}</link>
-    <description>探索算力资源的经济学原理与市场机制</description>
+    <description>微观算力经济学</description>
     <language>zh-CN</language>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
     <atom:link href="${baseUrl}/feed.xml" rel="self" type="application/rss+xml"/>
